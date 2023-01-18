e274944300ebde86f464adb97ae82ec60a063d9c
Alfred <alfred.ahonen@gmail.com>
2023-01-18 21:09:48 +0200

    hello world

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+import React from 'react';
 
 export default function App() {
+  const [text, setText] = React.useState('');
+
+  const buttonPressed = () => {
+    Alert.alert('Thank you', 'You SLAPPED ' + text + '!')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+      <Text>Hello, world!</Text>
+      <Button onPress={buttonPressed} title="Slap"></Button>
       <StatusBar style="auto" />
     </View>
   );

5c436ac904e997ace56bfba01cd46e2a21f64ef9
Alfred <alfred.ahonen@gmail.com>
2023-01-24 14:28:53 +0200

    laskin valmis

--- App.js
+++ App.js
@@ -1,24 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
-  const [text, setText] = React.useState('');
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [answer, setAnswer] = useState('');
 
-  const buttonPressed = () => {
-    Alert.alert('Thank you', 'You SLAPPED ' + text + '!')
+  const sum = () => {
+    setAnswer(num1 + num2);
+  }
+
+  const sub = () => {
+    setAnswer(num1 - num2);
   }
 
   return (
     <View style={styles.container}>
+      <Text style={{fontSize: 20}}>Result: {answer}</Text>
       <TextInput
-        style={{width: 200, borderColor: 'gray', borderWidth: 1}}
-        onChangeText={text => setText(text)}
-        value={text}
+        style={{width: 200, borderColor: 'gray', borderWidth: 1, fontSize: 20}}
+        onChangeText={text => setNum1(Number(text))}
+        value={num1}
+        placeholder="Number 1"
+        keyboardType="numeric"
       />
-      <Text>Hello, world!</Text>
-      <Button onPress={buttonPressed} title="Slap"></Button>
-      <StatusBar style="auto" />
+      <TextInput
+        style={{width: 200, borderColor: 'gray', borderWidth: 1, fontSize: 20}}
+        onChangeText={text => setNum2(Number(text))}
+        value={num2}
+        placeholder="Number 2"
+        keyboardType="numeric"
+      />
+      <View style={{width: 60, flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Button onPress={sum} title="+"></Button>
+        <Button onPress={sub} title="-"></Button>
+      </View>
     </View>
   );
 }
